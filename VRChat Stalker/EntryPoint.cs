8bcde0e0b31bf5e39496d212c62b049770f6a83e
neurowhai <tlsehdgus321@naver.com>
2018-07-07 22:37:00 -0600

    Show message about unhandled error.

--- VRChat Stalker/EntryPoint.cs
+++ VRChat Stalker/EntryPoint.cs
@@ -12,12 +12,25 @@ namespace VRChat_Stalker
         [STAThread]
         public static void Main(string[] args)
         {
-            var app = new App();
-            app.InitializeComponent();
-            
-            LoadTheme();
-            
-            app.Run();
+            try
+            {
+                var app = new App();
+                app.InitializeComponent();
+
+                LoadTheme();
+
+                app.Run();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show($@"There was a problem that could not be handled.
+처리하지 못한 문제가 발생하였습니다.
+Please capture the contents below.
+아래 내용을 캡쳐하여 보내주세요.
+
+{e.Message}
+{e.StackTrace}", "Error!");
+            }
         }
 
         private static void LoadTheme()
